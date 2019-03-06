ca3d00ae89cf7847cc892afddfdeb7380e712893
Arjit Chaudhary <arjit.c@gmail.com>
2019-03-07 04:16:36 +0530

    Update load.sh

--- load.sh
+++ load.sh
@@ -7,6 +7,8 @@ vzpkg install template centos-7-x86_64
 vzpkg install template debian-7.0-x86_64
 vzpkg install template debian-8.0-x86_64
 vzpkg install template debian-9.0-x86_64
+vzpkg install template ubuntu-16.04-x86_64
+vzpkg install template ubuntu-18.04-x86_64
 
 #cache the templates
 vzpkg create cache centos-6-x86_64
@@ -14,6 +16,8 @@ vzpkg create cache centos-7-x86_64
 vzpkg create cache debian-7.0-x86_64
 vzpkg create cache debian-8.0-x86_64
 vzpkg create cache debian-9.0-x86_64
+vzpkg create cache ubuntu-16.04-x86_64
+vzpkg create cache ubuntu-18.04-x86_64
 
 clear
 echo Done.
