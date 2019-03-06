b852fed06353c9d45b2ab622907d4554bb105424
Arjit Chaudhary <arjit.c@gmail.com>
2019-03-07 04:14:05 +0530

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo Load templates
+echo Install and Cache OpenVZ 7 Templates
 read -p "press [enter] to start..."
+#install the templates
+vzpkg install template centos-6-x86_64
+vzpkg install template centos-7-x86_64
+vzpkg install template centos-6-x86_64
+vzpkg install template centos-6-x86_64
+vzpkg install template centos-6-x86_64
+
+#cache the templates
+vzpkg create cache centos-6-x86_64
+vzpkg create cache centos-7-x86_64
 
 clear
 echo Done.
