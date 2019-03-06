e4de9d9c36af3bc64b0eba9fdcfff032e004594a
Arjit Chaudhary <arjit.c@gmail.com>
2019-03-07 04:15:33 +0530

    Update load.sh

--- load.sh
+++ load.sh
@@ -4,13 +4,16 @@ read -p "press [enter] to start..."
 #install the templates
 vzpkg install template centos-6-x86_64
 vzpkg install template centos-7-x86_64
-vzpkg install template centos-6-x86_64
-vzpkg install template centos-6-x86_64
-vzpkg install template centos-6-x86_64
+vzpkg install template debian-7.0-x86_64
+vzpkg install template debian-8.0-x86_64
+vzpkg install template debian-9.0-x86_64
 
 #cache the templates
 vzpkg create cache centos-6-x86_64
 vzpkg create cache centos-7-x86_64
+vzpkg create cache debian-7.0-x86_64
+vzpkg create cache debian-8.0-x86_64
+vzpkg create cache debian-9.0-x86_64
 
 clear
 echo Done.
